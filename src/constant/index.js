de44097eb50497c726c8a17586849da0d91b9e58
eyad <eyadmuhammad90@gmail.com>
2024-03-07 02:17:59 +0200

    fixed bugs

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,6 +1,6 @@
 export const navLinks = [
-  { id: "1", to: "/", name: "All" },
-  { id: "2", to: "/personal", name: "Personal" },
-  { id: "3", to: "/home", name: "Home" },
-  { id: "4", to: "/business", name: "Business" },
+  { id: "1", to: "/notely", name: "All" },
+  { id: "2", to: "/notely/personal", name: "Personal" },
+  { id: "3", to: "/notely/home", name: "Home" },
+  { id: "4", to: "/notely/business", name: "Business" },
 ];
