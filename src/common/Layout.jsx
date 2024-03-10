3b2eee804d7278f52a72350a56dbe20249301ea1
eyad <eyadmuhammad90@gmail.com>
2024-03-11 01:56:06 +0200

    fixed bugs

--- src/common/Layout.jsx
+++ src/common/Layout.jsx
@@ -4,11 +4,20 @@ import Nav from "../Components/Nav";
 import { useSelector } from "react-redux";
 import AddNote from "../Components/AddNote";
 import DeletNote from "../Components/DeletNote";
+import { useEffect } from "react";
+import UpdateNote from "../Components/UpdateNote";
 
 const Layout = () => {
+  const showUpdate = useSelector(({ updateNote }) => updateNote.isOpen);
+  const timeData = useSelector(({ updateNote }) => updateNote.time);
   const showAdd = useSelector(({ addNote }) => addNote);
   const { show } = useSelector(({ deleteNote }) => deleteNote);
 
+  useEffect(() => {
+    showAdd || showUpdate || show
+      ? document.body.classList.add("overflow-hidden")
+      : document.body.classList.remove("overflow-hidden");
+  }, [showAdd, showUpdate, show]);
   return (
     <>
       <div className="min-h-screen bg-[#EEEEEE]">
@@ -21,11 +30,12 @@ const Layout = () => {
         </div>
       </div>
       {showAdd && <AddNote />}
-      {showAdd && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-20" />
-      )}
+
       {show && <DeletNote />}
-      {show && (
+
+      {showUpdate && <UpdateNote time={timeData} />}
+
+      {(showUpdate || showAdd || show) && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-20" />
       )}
     </>
